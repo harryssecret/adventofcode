d25216010da58e39ccf4cfb8a6217dcd31c5d3cc
kity <geekcat@protonmail.com>
2022-12-07 21:00:13 +0100

    updated tests with pt2

--- day-3/advent_test.py
+++ day-3/advent_test.py
@@ -1,8 +1,19 @@
-from main import find_item_priority, find_mistake, find_good_rucksack
+from main import find_item_priority, find_mistake
+from part2 import create_elves_group, find_matching_char
 import unittest
 
 
 class TestAdventMethods(unittest.TestCase):
+
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        self.test_list = [
+            ['vJrwpWtwJgWrhcsFMMfFFhFp',
+                'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL', 'PmmdzqPrVvPwwTWBwg'],
+            ['wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+                'ttgJtRGJQctTZtZT', 'CrZsJsPPZsGzwwsLwLmpwMDw']
+        ]
+
     def test_find_item_priority(self):
         self.assertEqual(find_item_priority("p"), 16)
 
@@ -10,3 +21,9 @@ class TestAdventMethods(unittest.TestCase):
         self.assertEqual(find_mistake("vJrwpWtwJgWrhcsFMMfFFhFp"), "p")
         self.assertEqual(find_mistake('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL'), "L")
         self.assertEqual(find_mistake('PmmdzqPrVvPwwTWBwg'), "P")
+
+    def test_list_splitting(self):
+        self.assertEqual(create_elves_group('test_data.txt'), self.test_list)
+
+    def test_find_duplicate_item(self):
+        self.assertEqual(find_matching_char(self.test_list[0]), "r")
