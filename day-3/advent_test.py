33d770114e248c31e5dbb27e23c24e80fae0e57e
kity <geekcat@protonmail.com>
2022-12-07 20:10:32 +0100

    used unittest library for vscode support

--- day-3/advent_test.py
+++ day-3/advent_test.py
@@ -1,6 +1,12 @@
 from main import find_item_priority, find_mistake, find_good_rucksack
+import unittest
 
-assert find_item_priority("p") == 16
-assert find_mistake("vJrwpWtwJgWrhcsFMMfFFhFp") == "p"
-assert find_mistake('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL') == 'L'
-assert find_mistake('PmmdzqPrVvPwwTWBwg') == 'P'
+
+class TestAdventMethods(unittest.TestCase):
+    def test_find_item_priority(self):
+        self.assertEqual(find_item_priority("p"), 16)
+
+    def test_find_mistakes(self):
+        self.assertEqual(find_mistake("vJrwpWtwJgWrhcsFMMfFFhFp"), "p")
+        self.assertEqual(find_mistake('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL'), "L")
+        self.assertEqual(find_mistake('PmmdzqPrVvPwwTWBwg'), "P")
