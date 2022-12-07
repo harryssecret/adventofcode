9ac0cf45befbae8ae1e840d53106ff6fe6567e85
kity <geekcat@protonmail.com>
2022-12-07 20:04:40 +0100

    changed import filename

--- day-3/advent_test.py
+++ day-3/advent_test.py
@@ -1,4 +1,4 @@
-from adventofcode3 import find_item_priority, find_mistake, find_good_rucksack
+from main import find_item_priority, find_mistake, find_good_rucksack
 
 assert find_item_priority("p") == 16
 assert find_mistake("vJrwpWtwJgWrhcsFMMfFFhFp") == "p"
