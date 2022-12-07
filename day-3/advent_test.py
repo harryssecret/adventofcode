e4c0dfbcff524ab4e5a7744b5d4abdbd7d2a24b1
harry <37616962+harryssecret@users.noreply.github.com>
2022-12-07 16:04:49 +0100

    added day-3 solution (missing 2nd part)

---
+++ day-3/advent_test.py
@@ -0,0 +1,6 @@
+from adventofcode3 import find_item_priority, find_mistake, find_good_rucksack
+
+assert find_item_priority("p") == 16
+assert find_mistake("vJrwpWtwJgWrhcsFMMfFFhFp") == "p"
+assert find_mistake('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL') == 'L'
+assert find_mistake('PmmdzqPrVvPwwTWBwg') == 'P'
--- day-3/main.py
+++ day-3/main.py
@@ -1,1 +1,34 @@
+import string
 
+def parse_input(filename: str):
+    with open(filename) as f:
+        for line in f:
+            yield line
+
+def find_item_priority(item: str):
+    return string.ascii_letters.find(item) + 1
+    
+def main() -> None:
+    final_priority = 0
+    for rucksack in find_good_rucksack('puzzle.txt'):
+            mistake = find_mistake(rucksack)
+            final_priority += find_item_priority(mistake)
+    print(final_priority)
+
+def find_good_rucksack(filename: str):
+    for rucksack in parse_input(filename):
+            rucksack = rucksack.replace("\n", "")
+            if len(rucksack) % 2 == 0:
+                yield rucksack
+
+
+def find_mistake(rucksack: str):
+    half_size = int(len(rucksack) / 2)
+    first_half = rucksack[:half_size]
+    second_half = rucksack[half_size:]
+    for char in first_half:
+        if second_half.find(char) != -1:
+            return char
+
+if __name__ == "__main__":
+    main()
