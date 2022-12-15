04a338244c91786bd6d7a42453a6b5d0217e1914
kity <geekcat@protonmail.com>
2022-12-15 22:30:55 +0100

    wrote some tests

--- day-5/giant_cargo_test.py
+++ day-5/giant_cargo_test.py
@@ -1,7 +1,19 @@
 import unittest
+from main import GiantCargo
 
 
 class GiantCargoTests(unittest.TestCase):
 
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName)
+        cargo = GiantCargo()
+        cargo.read_data_from_file("test_data.txt")
+        cargo.parse_cargo()
+
     def test_giant_cargo_data_reading(self):
-        self.assertEqual()
+        self.assertListEqual(self.cargo.cargo, [[' ', 'D', ' '], ['N', 'C', ' '], [
+                             'Z', 'M', 'P'], ['1', '2', '3']])
+
+    def test_giant_cargo_instructions(self):
+        self.assertListEqual(self.cargo.cargo, [[' ', ' ', 'Z'], [
+                             ' ', ' ', 'N'], [' ', ' ', 'D'], ['C', 'M', 'P']])
