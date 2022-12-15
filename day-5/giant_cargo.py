338459fcd3ccc7dd9ce5e6b23a61889bde5c41c5
kity <geekcat@protonmail.com>
2022-12-15 22:30:34 +0100

    puzzle cargo parsing

--- day-5/giant_cargo.py
+++ day-5/giant_cargo.py
@@ -1,17 +1,43 @@
 class GiantCargo:
+    def __init__(self) -> None:
+        self.lines = []
+
     def read_data_from_file(self, filename: str):
-        print("aa")
         with open(filename) as f:
             for line in f:
                 if line == "\n":
-                    print("WAAAA")
+                    break
+                self.lines.append(line.replace("\n", ""))
 
-    def parse_cargo(self):
-        lines = []
-        for line in self.read_data_from_file():
-            pass
+    def parse_cargo(self) -> None:
+        parsed_lines = []
+        for l in self.lines:
+            actual_line = []
+            for i in range(1, len(l), 4):
+                actual_line.append(l[i])
+            parsed_lines.append(actual_line)
+        self.cargo = parsed_lines
+        self.size = len(parsed_lines[-1])
+
+    def new_line(self):
+        self.cargo.insert(0, ['' * self.size])
+
+    def move_crate(self, number: int, from_i: int, to_i: int) -> None:
+        for i in range(number):
+            current_list = self.cargo[i]
+            if self.isCrateHere(i, to_i):
+                pass
+            self.cargo[i] = current_list
+        pass
+
+    def isCrateHere(self, deep: int, position: int) -> bool:
+        return self.cargo[deep][position] != ' '
+
+    def show(self):
+        for l in self.cargo:
+            print(l)
 
 
 class Crate:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, letter: str) -> None:
+        self.name = letter
--- day-5/main.py
+++ day-5/main.py
@@ -3,7 +3,9 @@ from giant_cargo import GiantCargo
 
 def main() -> None:
     cargo = GiantCargo()
-    cargo.read_data_from_file("puzzle.txt")
+    cargo.read_data_from_file("test_data.txt")
+    cargo.parse_cargo()
+    cargo.show()
 
 
 if __name__ == "__main__":
